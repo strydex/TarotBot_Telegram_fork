c692a15faf5bdaeeec064866ec26fe1512ccf78b
Strydex <143320768+strydex@users.noreply.github.com>
2024-07-24 16:32:54 +0300

    Example config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = 'PLACE YOUR TELEGRAM BOT TOKEN HERE'
+PAYMENTS_TOKEN = 'PLACE YOUR PAYMENTS BOT TOKEN HERE'
+
+#You can also use Stripe Test Payments Processor just for testing, it doesn't need processing real bank cards.
