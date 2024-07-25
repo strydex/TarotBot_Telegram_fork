29d6cfd3faaf1f12dd52f58245d4fcba9f0bf151
Strydex <143320768+strydex@users.noreply.github.com>
2024-07-25 18:05:15 +0300

    Channels subscriptions update

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 BOT_TOKEN = 'PLACE YOUR TELEGRAM BOT TOKEN HERE'
 PAYMENTS_TOKEN = 'PLACE YOUR PAYMENTS BOT TOKEN HERE'
+CHANNELS = [
+    ["Name of a channel 1", "channel id", "channel link"],
+    ["Name of a channel 2", "channel id", "channel link"],
+]
+
+NOT_SUB_MESSAGE = "Бот полностью бесплатный благодаря спонсорам, для доступа к боту необходимо подписаться на их каналы"
 
 #You can also use Stripe Test Payments Processor just for testing, it doesn't need processing real bank cards.
