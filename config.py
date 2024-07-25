245abe97e1883a9a5aa201dd53706b0a628b572e
Strydex <143320768+strydex@users.noreply.github.com>
2024-07-25 18:59:55 +0300

    Channels subscriptions update

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ CHANNELS = [
     ["Name of a channel 2", "channel id", "channel link"],
 ]
 
-NOT_SUB_MESSAGE = "Бот полностью бесплатный благодаря спонсорам, для доступа к боту необходимо подписаться на их каналы"
+NOT_SUB_MESSAGE = "<b>Привет!</b> В этом боте ты можешь получить расклад на любую тему! Бот полностью бесплатный благодаря спонсорам, для доступа к боту необходимо подписаться на их каналы. После подписки нажмите кнопку 'Я подписался' и перезапустите бота, отправив команду /start"
 
 #You can also use Stripe Test Payments Processor just for testing, it doesn't need processing real bank cards.
